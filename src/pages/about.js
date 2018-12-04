9b75b4cb7339909ef9d0a6d43969a5bd55db54ec
Marcelo Pastorino <code@softwaredeveloper.io>
2018-12-04 03:03:06 +0100

    m3-c4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
   <div>
     <h1>This is the about page</h1>
+    <div>
+      <Link to="/">Home</Link> | <Link to="/about">About me</Link>
+    </div>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
