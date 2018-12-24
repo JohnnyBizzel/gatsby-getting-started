b98ad3e079b301098679ec5e3c4ef40bbb32f004
Marcelo Pastorino <code@softwaredeveloper.io>
2018-12-24 18:19:20 +0100

    m4-c2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,10 @@
 import React from "react"
+import styles from './404.module.css';
 
 export default () => (
   <div>
-    <h1>Page not found</h1>
-    <p>
+    <h1 className={styles.header}>Page not found</h1>
+    <p className={styles.errorMessage}>
       The page you are looking for does not exists.
     </p>
   </div>
