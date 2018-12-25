bd820e956c82d92be58a8bb630cf0abec9f2a817
Marcelo Pastorino <code@softwaredeveloper.io>
2018-12-25 16:08:18 +0100

    m4-c4

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+
+   plugins: [
+       'gatsby-plugin-sass'
+   ]
+
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
 import React from "react"
-import styles from './404.module.css';
+import styles from './404.module.scss';
 
 export default () => (
-  <div>
+  <div className={styles.content}>
     <h1 className={styles.header}>Page not found</h1>
     <p className={styles.errorMessage}>
       The page you are looking for does not exists.
