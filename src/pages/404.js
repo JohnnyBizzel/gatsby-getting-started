0a9d8d020b293a5dadeebad69aea12fdcc5784b7
Marcelo Pastorino <code@softwaredeveloper.io>
2018-12-26 14:59:03 +0100

    m5-c3

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,16 @@
-import React from "react"
+import React from 'react'
 import styles from './404.module.scss';
+import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
 export default () => (
-  <div className={styles.content}>
-    <h1 className={styles.header}>Page not found</h1>
-    <p className={styles.errorMessage}>
-      The page you are looking for does not exists.
+  <Layout>
+    <div className={styles.content}>
+      <h1 className={styles.header}>Page not found</h1>
+      <p className={styles.errorMessage}>
+        The page you are looking for does not exists.
     </p>
-  </div>
+    <Link to='/'>Home</Link>
+    </div>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 export default () => (
-   <div>
+   <Layout>
     <h1>This is the index page</h1>
     <div>
       <Link to="/">Home</Link> | <Link to="/about">About me</Link>
@@ -10,5 +11,5 @@ export default () => (
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-  </div>
+  </Layout>
 )
