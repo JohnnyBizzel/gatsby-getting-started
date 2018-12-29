7723a01caf5b0ea6e66096b0c9a2df457d065d0b
Marcelo Pastorino <code@softwaredeveloper.io>
2018-12-29 17:24:46 +0100

    m7-c4

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
             resolve: 'gatsby-source-filesystem',
             options: {
               name: 'files',
-              path: `${__dirname}/src/pages`,
+              path: `${__dirname}/src/markdown`,
             },
         },
     ]
