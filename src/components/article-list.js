e55177be3795179299af1f2261d3fd8a9148f0be
Marcelo Pastorino <code@softwaredeveloper.io>
2018-12-29 23:56:37 +0100

    m7-c6

---
+++ src/components/article-list.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby'
+import Article from '../components/article'
+
+export default () => (
+   <StaticQuery
+
+      query = { graphql `query {
+         allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+           totalCount
+           edges {
+             node {
+               id
+               frontmatter {
+                 title
+                 image
+                 keywords
+                 date(formatString: "MMMM YYYY")
+               }
+               excerpt
+             }
+           }
+         }
+       }`
+
+      }
+
+      render = { data => (
+         <div>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+               <Article id={node.id}
+                  to='/'
+                  keywords={node.frontmatter.keywords}
+                  title={node.frontmatter.title}
+                  date={node.frontmatter.date}
+                  excerpt={node.excerpt} />
+            ))}
+         </div>
+      )}
+
+   />
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,15 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Title from '../components/title'
+import ArticleList from '../components/article-list'
 
-export default ({data}) => (
+export default () => (
   <Layout>
     <Title text='Welcome'/>
-    <div>
-      <Link to='/'>Home</Link> | <Link to='/about'>About me</Link>
-    </div>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-
-    <div>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <article>
-          <div>
-            <strong>{node.frontmatter.title}</strong>
-          </div>
-          <div>
-            {node.excerpt}
-          </div>
-        </article>
-      ))}
-    </div>
-
-
+    <ArticleList />
   </Layout>
 )
 
-export const query = graphql `query {
-  allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
-    totalCount
-    edges {
-      node {
-        id
-        frontmatter {
-          title
-          image
-          date(formatString: "MMMM YYYY")
-        }
-        excerpt
-      }
-    }
-  }
-}`
