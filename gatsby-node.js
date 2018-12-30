4b692bf1b47a53b2e3a2e8ddf5d30b186536d68f
Marcelo Pastorino <code@softwaredeveloper.io>
2018-12-30 20:22:12 +0100

    m8-c6

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const { createFilePath } = require('gatsby-source-filesystem')
+const path = require('path')
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
    if (node.internal.type === 'MarkdownRemark') {
@@ -10,4 +11,36 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
          value: slug,
       })
    }
+}
+
+// Create post pages programmatically
+exports.createPages = ({ graphql, actions }) => {
+   const { createPage } = actions
+
+   return new Promise(resolve => {
+      graphql(`
+      {
+         allMarkdownRemark {
+            edges {
+               node {
+                  fields {
+                     slug
+                  }
+               }
+            }
+         }
+      }`
+      ).then(result => {
+         result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+               path: node.fields.slug,
+               component: path.resolve('./src/templates/post.js'),
+               context: {
+                 slug: node.fields.slug,
+               },
+             })
+         })
+         resolve()
+      })
+   })
 }
